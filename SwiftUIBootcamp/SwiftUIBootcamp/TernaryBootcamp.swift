3589f711185facdc77a42db08d67894907f65f04
Yoonji Lee <yooonji08@naver.com>
2024-02-18 14:02:10 +0900

    [24강] 삼항 연산자를 사용하여 뷰 제어하기

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/TernaryBootcamp.swift
@@ -0,0 +1,35 @@
+//
+//  TernaryBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by 이윤지 on 2/18/24.
+//
+
+import SwiftUI
+
+struct TernaryBootcamp: View {
+    @State var  isStartingState: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button: \(isStartingState.description)") {
+                isStartingState.toggle()
+            }
+            
+            // 삼항 연산자를 사용하여 각 컴포넌트의 값 제어하기
+            Text(isStartingState ? "STARTING STATE" : "ENDING STATE")
+            
+            RoundedRectangle(cornerRadius: isStartingState ? 25 : 0)
+                .fill(isStartingState ? Color.mint : Color.purple)
+                .frame(
+                    width: isStartingState ? 200 : 50,
+                    height: isStartingState ? 400 : 50)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    TernaryBootcamp()
+}
